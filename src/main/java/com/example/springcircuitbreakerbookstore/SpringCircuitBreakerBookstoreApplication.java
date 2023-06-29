f3751f561b8b28c45fd98b82ef92ac21104b17b4
Siddharth Pathak <siddharthpathak415@gmail.com>
2023-06-29 01:31:08 -0700

    added Rest Controller & created endpoint for suggested books

--- src/main/java/com/example/springcircuitbreakerbookstore/SpringCircuitBreakerBookstoreApplication.java
+++ src/main/java/com/example/springcircuitbreakerbookstore/SpringCircuitBreakerBookstoreApplication.java
@@ -2,10 +2,18 @@ package com.example.springcircuitbreakerbookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
+@RestController
 @SpringBootApplication
 public class SpringCircuitBreakerBookstoreApplication {
 
+    @RequestMapping(value = "/suggested")
+    public Mono<String> readingList(){
+        return Mono.just("Intro to Algorithms, Theory of Computation ");
+    }
     public static void main(String[] args) {
         SpringApplication.run(SpringCircuitBreakerBookstoreApplication.class, args);
     }
