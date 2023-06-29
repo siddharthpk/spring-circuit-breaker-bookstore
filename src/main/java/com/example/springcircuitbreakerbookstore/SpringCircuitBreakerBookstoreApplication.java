8680a4a006830a98208c9c4928a2414220e43568
Sid Pathak <siddharthpathak415@gmail.com>
2023-06-29 01:23:15 -0700

    intial commit

---
+++ src/main/java/com/example/springcircuitbreakerbookstore/SpringCircuitBreakerBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcircuitbreakerbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCircuitBreakerBookstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCircuitBreakerBookstoreApplication.class, args);
+    }
+
+}
